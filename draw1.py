d6eb61185a3e7ea705428717c0876d81cec7f1e6
yuye <3238918486@qq.com>
2024-09-01 18:17:41 +0800

    Completed draw scripts with the seaborn lib (There are just some brief examples, you should justify it to fit your own need)

--- draw1.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import seaborn as sns
-import pandas as pd
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-sns.set(style="whitegrid")
-tips = sns.load_dataset("tips")
-sns.relplot(x="total_bill", y="tip", col="time", hue="smoker", size="size", data=tips)
-plt.show()
