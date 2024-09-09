196ebefde8e862d05c8081fba5074d5452ce789a
yuye <3238918486@qq.com>
2024-09-09 21:58:47 +0800

    Fixed K-means for 2024 CUMCM

--- K-means.py
+++ K-means.py
@@ -1,11 +1,16 @@
 from sklearn.cluster import KMeans  
 import numpy as np  
+import pandas as pd
 import matplotlib.pyplot as plt  
-  
-# 假设我们有一些二维数据  
-data = np.random.randint(0,10,size=[100,2]) + np.random.random((100,2))
-print(data)
-  
+
+# 设置中文字体  
+plt.rcParams['font.sans-serif'] = ['SimHei']  # 使用黑体  
+plt.rcParams['axes.unicode_minus'] = False    # 解决负号显示问题    
+# 读取xlsx文件  
+data = pd.read_excel('data.xlsx',sheet_name='Sheet2',usecols=['作物编号','每亩利润'],engine='openpyxl')  
+df = data.values
+# 查看数据  
+print(df)
 # 设置聚类数K  
 K = 4
   
@@ -13,8 +18,8 @@ K = 4
 kmeans = KMeans(n_clusters=K, random_state=0)  
   
 # 对数据进行拟合和预测  
-kmeans.fit(data)  
-labels = kmeans.predict(data)  
+kmeans.fit(df)  
+labels = kmeans.predict(df)  
 centroids = kmeans.cluster_centers_  
   
 # 打印聚类中心和标签  
@@ -24,9 +29,9 @@ print("Labels:")
 print(labels)  
  
 # 可视化结果  
-plt.scatter(data[:, 0], data[:, 1], c=labels, cmap='viridis')  
+plt.scatter(data['作物编号'],data['每亩利润'],c=labels, cmap='viridis')
 plt.scatter(centroids[:, 0], centroids[:, 1], c='red', s=300, alpha=0.5)  
 plt.title('K-means Clustering')  
-plt.xlabel('Feature 1')  
-plt.ylabel('Feature 2')  
+plt.xlabel('编号')  
+plt.ylabel('每亩售价')  
 plt.show()
